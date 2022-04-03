fde0ad65d4053a76ab70e45dfe532ec0e56e5b07
Weronika <w.szczegodzinska@gmail.com>
2022-04-03 23:14:45 +0200

    comments and visualisation update

--- emo-analysis.py
+++ emo-analysis.py
@@ -3,8 +3,14 @@ import string
 import pandas as pd
 import spacy
 from stop_words import get_stop_words
+import matplotlib.pyplot as plt
+import numpy as np
+
+''' http://zil.ipipan.waw.pl/SpacyPL '''
 
 nlp = spacy.load('pl_spacy_model')
+
+''' wczytywanie stop words '''
 
 stop_words = get_stop_words('polish')
 
@@ -15,13 +21,14 @@ for l in lines:
     polish_stopwords.append(l.strip())
 f.close()
 
+
+''' wczytywanie korpusów '''
+
 happy = {}
 sadness = {}
 anger = {}
 fearness = {}
 disgust = {}
-
-data = {}
 
 with open('corpuses/nawl_emo_cat.csv', 'r', encoding='utf8') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
@@ -63,7 +70,14 @@ sadness_values = []
 fear_values = []
 disgust_values = []
 
-df = pd.read_csv('past/premises.csv')
+hap_count = 0
+ang_count = 0
+sad_count = 0
+fea_count = 0
+dis_count = 0
+none_count = 0
+
+df = pd.read_csv('original-model/premises.csv')
 emo_an = pd.DataFrame(df)
 
 for i in emo_an['text']:
@@ -78,11 +92,15 @@ for i in emo_an['text']:
     for char in string.punctuation:
         i = i.replace(char, '').strip().lower()
 
+    ''' pętla bierze tokeny z danej przesłanki i jeżeli nie należy on do stop words, dodaje do zbioru '''
+
     tokens = []
     for token in nlp(i):
         if not token.lemma_ in polish_stopwords and not token.lemma_ in stop_words:
             tokens.append(token.lemma_)
             words_num = words_num + 1
+
+    ''' obliczanie wartości emocji dla słów w zbiorze '''
 
     for token in tokens:
         if token in happy.keys():
@@ -98,20 +116,30 @@ for i in emo_an['text']:
     fear_values.append(fear_val)
     disgust_values.append(dis_val)
 
+    ''' przypisywanie odpowiedniej kategorii zależnie od najwyższej wartości emocji '''
+
     maks = max(hap_val/words_num, sad_val/words_num, angry_val/words_num, fear_val/words_num, dis_val/words_num)
     if maks >= 0.5:
         if maks == hap_val/words_num:
             labels.append('HAP')
+            hap_count = hap_count + 1
         elif maks == angry_val / words_num:
             labels.append('ANG')
+            ang_count = ang_count + 1
         elif maks == sad_val/words_num:
             labels.append('SAD')
+            sad_count = sad_count + 1
         elif maks == fear_val / words_num:
             labels.append('FEA')
+            fea_count = fea_count + 1
         elif maks == dis_val / words_num:
             labels.append('DIS')
+            dis_count = dis_count + 1
     else:
         labels.append('None')
+        none_count = none_count + 1
+
+''' zapisywanie danych '''
 
 emo_an['hap_val'] = happy_values
 emo_an['ang_val'] = anger_values
@@ -120,4 +148,25 @@ emo_an['fea_val'] = fear_values
 emo_an['dis_val'] = disgust_values
 emo_an['EMO_lab'] = labels
 
+''' zapisywanie do pliku '''
+
 emo_an.to_csv('emotions-analysis.csv', index=False, encoding='utf-8')
+
+
+''' wizualizacja '''
+
+np.random.seed(19680801)
+plt.rcdefaults()
+fig, ax = plt.subplots()
+
+y_axis = ('HAP', 'ANG', 'SAD', 'FEA', 'DIS', 'None')
+x_axis = (hap_count, ang_count, sad_count, fea_count, dis_count, none_count)
+
+for i, v in enumerate(x_axis):
+    ax.text(v + 1, i + 0.06, str(v), color='black', size='small')
+
+plt.barh(y_axis, x_axis, align='center', color='maroon')
+ax.invert_yaxis() # labels read top-to-bottom
+ax.set_xlabel('Liczba przesłanek zakwalifikowanych do danej kategorii')
+ax.set_title('Analiza emocjonalna przesłanek')
+plt.savefig('plots/emo-an.png')
